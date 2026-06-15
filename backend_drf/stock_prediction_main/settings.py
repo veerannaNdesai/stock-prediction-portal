da034713f9b71dacead5a7bc3424b5e3455773fa
repo-Demo <iveerannadesai@gmail.com>
2026-06-15 14:06:16 +0530

    fixed the allowed hosts again

--- backend_drf/stock_prediction_main/settings.py
+++ backend_drf/stock_prediction_main/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=False,cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "*"
+]
 
 
 # Application definition
@@ -126,9 +128,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-CORS_ALLOWED_ORIGINS = [
-    '*',
-]
+CORS_ALLOWED_ORIGINS = True
 
 REST_FRAMEWORK = {
     
