b620d3bdd6d2fe686ffd60896d5b6c4b78a7f220
repo-Demo <iveerannadesai@gmail.com>
2026-06-15 13:24:46 +0530

    Dockerized backend and migrated to Neon PostgreSQL

--- backend_drf/stock_prediction_main/settings.py
+++ backend_drf/stock_prediction_main/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from decouple import config
 from datetime import timedelta
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,10 +84,9 @@ WSGI_APPLICATION = 'stock_prediction_main.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(
+        config("DATABASE_URL")
+    )
 }
 
 
